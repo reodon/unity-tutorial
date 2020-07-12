b649fd32e87df2dd7e1e4b39ba617a74626d38ed
reodon <foo@hoge>
2020-07-12 18:04:19 +0900

    update

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float m_speed;
+    private void Update()
+    {
+        var h = Input.GetAxis( "Horizontal" );
+	var v = Input.GetAxis( "Vertical" );
+	var velocity = new Vector3( h, v ) * m_speed;
+	transform.localPosition += velocity;
+    }
+}
